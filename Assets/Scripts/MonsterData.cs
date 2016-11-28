d77069562d313ac547f16059df09b234941e495b
polis89 <89polis@gmail.com>
2016-11-28 20:49:40 +0100

    First commit

---
+++ Assets/Scripts/MonsterData.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class MonsterLevel{
+	public int cost;
+	public GameObject visualisation;
+}
+
+public class MonsterData : MonoBehaviour {
+	public List<MonsterLevel> levels;
+
+	private MonsterLevel currentLevel;
+	public MonsterLevel CurrentLevel{
+		set{ 
+			currentLevel = value;
+			int currentLevelIndex = levels.IndexOf (currentLevel);
+			GameObject levelVisualisation = levels [currentLevelIndex].visualisation;
+			for (int i = 0; i < levels.Count; i++) {
+				if (levelVisualisation != null) {
+					if (currentLevelIndex == i) {
+						levels [i].visualisation.SetActive (true);
+					} else {
+						levels [i].visualisation.SetActive (false);
+					}
+				}
+			}
+		}
+		get{
+			return currentLevel;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnEnable(){
+		CurrentLevel = levels [0];
+	}
+
+	public MonsterLevel GetNextLevel(){
+		int currentLevelIndex = levels.IndexOf (currentLevel);
+		if (currentLevelIndex < levels.Count - 1) {
+			return levels [currentLevelIndex + 1];
+		} else {
+			return null;
+		}
+	}
+
+	public void IncreaseLevel(){
+		int currentLevelIndex = levels.IndexOf (currentLevel);
+		if (currentLevelIndex < levels.Count - 1) {
+			CurrentLevel = levels [currentLevelIndex + 1];
+		}
+	}
+}
---
+++ Assets/Scripts/PlaceMonster.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceMonster : MonoBehaviour {
+	public GameObject monsterPrefab;
+
+	private GameObject monster;
+	private GameManagerBehavior gameManagerBehavior;
+
+	// Use this for initialization
+	void Start () {
+		gameManagerBehavior = GameObject.Find ("GameManager").GetComponent<GameManagerBehavior> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUp(){
+		if (canPlaceMonster ()) {
+			monster = Instantiate (monsterPrefab, transform.position, Quaternion.identity) as GameObject;
+			AudioSource audioSource = GetComponent<AudioSource> ();
+			audioSource.PlayOneShot (audioSource.clip);
+			gameManagerBehavior.Gold -= monster.GetComponent<MonsterData> ().CurrentLevel.cost;
+		} else if (canUpgradeMonster ()) {
+			monster.GetComponent<MonsterData> ().IncreaseLevel();
+			AudioSource audioSource = GetComponent<AudioSource> ();
+			audioSource.PlayOneShot (audioSource.clip);
+			gameManagerBehavior.Gold -= monster.GetComponent<MonsterData> ().CurrentLevel.cost;
+		}
+	}
+
+	private bool canPlaceMonster(){
+		int cost = monsterPrefab.GetComponent<MonsterData> ().levels [0].cost;
+		return monster == null && gameManagerBehavior.Gold >= cost;
+	}
+
+	private bool canUpgradeMonster(){
+		if (monster != null) {
+			MonsterData mData = monster.GetComponent<MonsterData> ();
+			MonsterLevel mLevel = mData.GetNextLevel ();
+			if (mLevel != null) {
+				return gameManagerBehavior.Gold >= mLevel.cost;
+			}
+		}
+		return false;
+	}
+}
